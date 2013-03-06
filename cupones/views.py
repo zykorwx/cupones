4e7eec4f7f163a350b1863fd44e4efd5d396ab2f
Zykor <enrique.wx@gmail.com>
2013-03-06 13:59:37 -0600

    segundo cambio

--- cupones/views.py
+++ cupones/views.py
@@ -4,10 +4,5 @@ from django.template import RequestContext
 from cupon.models import Promocion
 
 def index(request):
-	total = Promocion.objects.count()
-	mitad = total // 2
-	if mitad == 0:
-		mitad = 1
-	promociones = Promocion.objects.all()[:mitad]
-	promociones1 = Promocion.objects.all()[mitad:total]
-	return render_to_response('cupones/index.html', {'promociones': promociones, 'promociones1':promociones1}, context_instance=RequestContext(request))
+	promociones = Promocion.objects.all()
+	return render_to_response('cupones/index.html', {'promociones': promociones}, context_instance=RequestContext(request))
