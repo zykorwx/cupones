53a21ba83ae3a751831a1e197ace2daa7da352f8
zykor <enrique.wx@gmail.com>
2013-03-08 03:33:46 +0000

    squere

--- usuario/models.py
+++ usuario/models.py
@@ -38,7 +38,7 @@ def new_users_handler(sender, user, response, details, **kwargs):
             try:
                 url = None
                 if sender == FacebookBackend:
-                    url = "http://graph.facebook.com/%s/picture?type=small" \
+                    url = "http://graph.facebook.com/%s/picture?type=square" \
                     % response["id"]
                 elif sender == TwitterBackend:
                  	url = response["profile_image_url"]
