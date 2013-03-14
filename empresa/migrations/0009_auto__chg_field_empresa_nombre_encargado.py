b1c98a0994375636f07f8ea6e4a86c1448942f73
Zykor <enrique.wx@gmail.com>
2013-03-14 02:58:07 -0600

    Corrigiendo descripcion en promociones

---
+++ empresa/migrations/0009_auto__chg_field_empresa_nombre_encargado.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Empresa.nombre_encargado'
+        db.alter_column(u'empresa_empresa', 'nombre_encargado', self.gf('django.db.models.fields.CharField')(max_length=50, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Empresa.nombre_encargado'
+        db.alter_column(u'empresa_empresa', 'nombre_encargado', self.gf('django.db.models.fields.TextField')(null=True))
+
+    models = {
+        u'empresa.empresa': {
+            'Meta': {'object_name': 'Empresa'},
+            'calle': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'colonia': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
+            'estado': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'giro': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'municipio': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'nombre_encargado': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'num_exterior': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'num_interior': ('django.db.models.fields.CharField', [], {'max_length': '8', 'null': 'True', 'blank': 'True'}),
+            'pagina_web': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'telefono': ('django.db.models.fields.CharField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'})
+        },
+        u'empresa.userempresa': {
+            'Meta': {'object_name': 'UserEmpresa'},
+            'empresa': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['empresa.Empresa']", 'unique': 'True'}),
+            'fecha_creacion': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'user': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'})
+        }
+    }
+
+    complete_apps = ['empresa']
