450584aada5c3d827b628bfe1c3019afefad0e11
Zykor <enrique.wx@gmail.com>
2013-03-23 01:35:12 -0600

    Falta mostrar los cupones por periodo y usar el metodo push dentro del admin para las notificaciones ... se acerca lo mas interesante xD

---
+++ public/static/js/empresa/promoEmpresa.js
@@ -0,0 +1,34 @@
+(function($){
+
+  calendario = function() {
+    $("#id_fecha_publicacion").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true });
+    $("#id_fecha_termino").datepicker({ dateFormat: 'yy-mm-dd', changeMonth: true, changeYear: true  });
+  };
+
+
+
+
+  mensajes = function(){
+	$(" #id_fecha_publicacion").on( "click", function( event ) {
+  	$('#mensaje_field').text('Introduzca la fecha en que desea que se publique la promocion. Este campo se puede dejar vacio.')});
+
+  $(" #id_fecha_termino").on( "click", function( event ) {
+    $('#mensaje_field').text('Introduzca la fecha en que desea que termine la promocion. Este campo se puede dejar vacio.')});
+
+  $(" #id_num_limite").on( "click", function( event ) {
+    $('#mensaje_field').html('Introduzca el numero limite de cupones que desea dar. Este campo se puede dejar vacio. <cite>Puede combinar el numero de cupones con la fecha de publicacion y fecha limite </cite>')});
+
+  $(" #id_estado").on( "click", function( event ) {
+    $('#mensaje_field').text('Elija si su promocion se publica o no. Si elije publicar cuando la promocion ya llego a su fecha final o al limite de sus cupones, forzara a su publicacion, o igual si ya no desea que se muestre la promocion tambien lo puede forzar.')});
+
+  $(" #id_descripcion").on( "click", function( event ) {
+    $('#mensaje_field').text('Escriba una breve descripcion sobre la promocion')});
+
+
+};    
+
+
+
+})(jQuery);
+
+
